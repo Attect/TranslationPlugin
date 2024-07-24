7cfb067a1a01dec288b05ab0b0ca4634389b8f21
Attect <attect@muka.app>
2024-07-24 15:00:59 +0800

    增加一个OpenAI GLM4模型，用于本地ollama翻译

--- src/main/kotlin/cn/yiiguxing/plugin/translate/trans/openai/OpenAiModel.kt
+++ src/main/kotlin/cn/yiiguxing/plugin/translate/trans/openai/OpenAiModel.kt
@@ -12,13 +12,15 @@ enum class OpenAiModel(val value: String, val modelName: String) {
     GPT_4("gpt-4", "GPT-4"),
     GPT_3_5_TURBO("gpt-3.5-turbo", "GPT-3.5 Turbo"),
     GPT_3_5_TURBO_1106("gpt-3.5-turbo-1106", "GPT-3.5 Turbo 1106"),
+    GLM4("glm4", "GLM4"),
+
 
     TTS_1("tts-1", "TTS-1"),
     TTS_1_HD("tts-1-hd", "TTS-1 HD");
 
     companion object {
         fun gptModels(): List<OpenAiModel> {
-            return OpenAiModel.values().filter { it.value.startsWith("gpt") }.toList()
+            return OpenAiModel.values().filter { it.value.startsWith("gpt") || it.value.startsWith("glm") }.toList()
         }
 
         fun ttsModels(): List<OpenAiModel> {
